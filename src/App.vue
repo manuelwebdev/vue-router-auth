b648262f2ffe2d14ac86641d2c41a36f61d8603b
manuelwebdev <mfewcar@gmail.com>
2022-04-28 12:57:39 -0600

    got vue router and google auth functioning.

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,65 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <h1>IsInit: {{ Vue3GoogleOauth.isInit }}</h1>
+  <button @click="handleClickSignIn" :disabled="!Vue3GoogleOauth.isInit || Vue3GoogleOauth.isAuthorized">sign in</button>
+  <button @click="handleClickSignOut" :disabled="!Vue3GoogleOauth.isAuthorized">sign out</button>
+  <router-view />
 </template>
+
+<script>
+import { inject, toRefs } from "vue";
+
+export default {
+  data() {
+    return {
+      user: "",
+    };
+  },
+  setup(props) {
+    const { isSignIn } = toRefs(props);
+    const Vue3GoogleOauth = inject("Vue3GoogleOauth");
+    const handleClickLogin = () => {};
+    return {
+      Vue3GoogleOauth,
+      handleClickLogin,
+      isSignIn,
+    };
+  },
+  methods: {
+    async handleClickSignIn() {
+      try {
+        const googleUser = await this.$gAuth.signIn();
+        if (!googleUser) {
+          return null;
+        }
+        console.log("googleUser", googleUser);
+        this.user = googleUser.getBasicProfile().getEmail();
+        console.log("getId", this.user);
+        console.log("getBasicProfile", googleUser.getBasicProfile());
+        console.log("getAuthResponse", googleUser.getAuthResponse());
+        console.log(
+          "getAuthResponse",
+          this.$gAuth.instance.currentUser.get().getAuthResponse()
+        );
+      } catch (error) {
+        //on fail do something
+        console.error(error);
+        return null;
+      }
+    },
+    async handleClickSignOut() {
+      try {
+        await this.$gAuth.signOut();
+        console.log("isAuthorized", this.Vue3GoogleOauth.isAuthorized);
+        this.user = "";
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+};
+</script>
 
 <style>
 #app {
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import GAuth from 'vue3-google-oauth2'
+const app = createApp(App)
 
-createApp(App).use(router).mount('#app')
+const gAuthOptions = { clientId: '921534492360-0p4o8gg4coi65c2s3d35hit1vfh7veoq.apps.googleusercontent.com' }
+
+app.use(router).use(GAuth, gAuthOptions).mount('#app')
